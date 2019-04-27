953c0c78b2f389b7da2804e858593f10f7885bad
Szewa <marcin91.szewczyk@gmail.com>
2019-04-27 13:09:27 +0200

    Initial commit

---
+++ src/main/java/pl/parser/nbp/MainClass.java
@@ -0,0 +1,9 @@
+package pl.parser.nbp;
+
+public class MainClass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/pl/parser/nbp/MainClassTest.java
@@ -0,0 +1,15 @@
+package pl.parser.nbp;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainClassTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isEqualTo(true);
+    }
+}
