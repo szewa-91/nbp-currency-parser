b1ca3d6d8669e6cf0f6f2a9f802d182e9de3da3c
Szewa <marcin91.szewczyk@gmail.com>
2019-05-01 22:46:36 +0200

    Fix issue with whitespace in first file name

--- src/main/java/pl/parser/nbp/snapshot/provider/NbpApiFileNamesProvider.java
+++ src/main/java/pl/parser/nbp/snapshot/provider/NbpApiFileNamesProvider.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -15,13 +16,21 @@ public class NbpApiFileNamesProvider implements FileNamesProvider {
     private static final String DIR_FILE = "https://www.nbp.pl/kursy/xml/dir.txt";
     private static final String FILENAME_PREFIX = "c";
     private static final int DATE_SUBSTRING_BEGIN_INDEX = 5;
+    private static final String UNNECESARY_WHITESPACE_PREFIX = "\uFEFF";
 
     @Override
     public Collection<String> getFileNames(LocalDate startDate, LocalDate endDate) {
         return getAllFileNames().stream()
+                .map(removeLeadingWhitespace())
                 .filter(filterByPrefix())
                 .filter(filterByDate(startDate, endDate))
                 .collect(Collectors.toList());
+    }
+
+    private Function<String, String> removeLeadingWhitespace() {
+        return fileName -> fileName.startsWith(UNNECESARY_WHITESPACE_PREFIX)
+                ? fileName.substring(1)
+                : fileName;
     }
 
     private Collection<String> getAllFileNames() {
--- src/test/java/pl/parser/nbp/snapshot/provider/NbpApiFileNamesProviderTest.java
+++ src/test/java/pl/parser/nbp/snapshot/provider/NbpApiFileNamesProviderTest.java
@@ -10,13 +10,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 // this test uses external connection to NBP service,
 // may fail in case of no connection or changes in files
 public class NbpApiFileNamesProviderTest {
-    private static final LocalDate START_DATE = LocalDate.of(2019, 1, 10);
-    private static final LocalDate END_DATE = LocalDate.of(2019, 1, 15);
+    private static final LocalDate START_DATE = LocalDate.of(2019, 1, 2);
+    private static final LocalDate START_DATE_BROKEN_NAME = LocalDate.of(2019, 1, 1);
+    private static final LocalDate END_DATE = LocalDate.of(2019, 1, 7);
+
     private NbpApiFileNamesProvider nbpApiFileNamesProvider = new NbpApiFileNamesProvider();
 
     @Test
     public void shouldGetFileNamesFromNbpAndFilter() {
         Collection<String> fileNames = nbpApiFileNamesProvider.getFileNames(START_DATE, END_DATE);
-        assertThat(fileNames).contains("c007z190110", "c008z190111", "c009z190114", "c010z190115");
+
+        assertThat(fileNames).contains("c002z190103", "c003z190104", "c004z190107");
+    }
+
+    @Test
+    public void shouldFixBrokenFileName() {
+        Collection<String> fileNames = nbpApiFileNamesProvider.getFileNames(START_DATE_BROKEN_NAME, END_DATE);
+
+        assertThat(fileNames).contains("c001z190102", "c002z190103", "c003z190104", "c004z190107");
     }
 }
